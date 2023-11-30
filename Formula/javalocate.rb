8fc988b57726f35f36974bdf569ebf35832d8684
David Meikle <david@meikle.io>
2023-11-30 11:40:34 +0000

    Updated for v0.7.0 release

--- Formula/javalocate.rb
+++ Formula/javalocate.rb
@@ -5,9 +5,9 @@ class Javalocate < Formula
 
   desc "Command line utility to find JVM versions on macOS and Linux" 
   homepage "https://github.com/dameikle/javalocate"
-  url "https://github.com/dameikle/javalocate/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "073d61f43660fe35851201fb9946cc4eba2789a8dc25ad353cd56d78f9f9d18f"
-  version "0.6.0"
+  url "https://github.com/dameikle/javalocate/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "41ea06fdedf7661ad14f7b228590bdd8454eb08382fecfb40df8aae21bee6ac2"
+  version "0.7.0"
   depends_on "rust" => :build
 
 def install
